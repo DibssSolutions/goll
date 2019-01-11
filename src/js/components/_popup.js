06ce0c4f87b5c4b642b035e8f4014a3ea1f7dbd7
Ihor Liubko <liub41k69@gmail.com>
2019-01-11 17:48:51 +0200

    popup x6 done

--- src/js/components/_popup.js
+++ src/js/components/_popup.js
@@ -4,13 +4,21 @@ trigger.on('click', function(e) {
   e.preventDefault();
   var that = $(this);
   var id = that.data('modal-trigger');
-
   var modal = $(`[data-modal=${id}]`);
   modal.addClass(OPEN);
 });
 
-// var popup = $('[data-modal]');
-// popup.on('click', function() {
-//   $(this).removeClass(OPEN);
-// });
+var popup = $('[data-modal]');
+popup.on('click', function(e) {						// событие клика по блоку с атрибутом [data-modal]
+  var block = $('.js-popup-wrap');					// элемент к которому применяем условие
+  if (!block.is(e.target)									// проверка условия если клик был не по нашему блоку
+  	&& block.has(e.target).length === 0) {// проверка условия если клик не по его дочерним элементам
+  	$(this).removeClass(OPEN);						// усли условия выполняются 	 убираеться класс is-open
+  }
+});
 
+$('[data-modal]').each(function() {
+  var this_ = $(this);
+  var itemLength = this_.find('.features').length;
+  this_.attr('data-length-features', itemLength);
+});
