1886df05a5a77354fa2c85aada898146b576dfc5
Ihor Liubko <liub41k69@gmail.com>
2019-01-17 13:17:08 +0200

    toggle reworked

--- src/js/components/_toggle.js
+++ src/js/components/_toggle.js
@@ -1,19 +1,19 @@
-import {OPEN, BODY} from '../constants.js';
+import {OPEN, BODY, OVERFLOW_HIDDEN} from '../constants.js';
 
 var burger = $('.js-toggle');
 var burgerIcon = $('.js-toggle-icon');
 var menu = $('.js-menu');
+var header = $('.js-header');
+var logo = $('.js-logo');
+
 burger.on('click', function() {
   burgerIcon.toggleClass(OPEN);
   menu.toggleClass(OPEN);
+  header.toggleClass(OPEN);
+  BODY.toggleClass(OVERFLOW_HIDDEN);
 });
 
-BODY.on('click', function() {
-  menu.removeClass(OPEN);
-  burgerIcon.removeClass(OPEN);
-});
-
-var header = $('.js-header');
-header.on('click', function(e) {
-  e.stopPropagation();
-});
+// var header = $('.js-header');
+// header.on('click', function(e) {
+//   e.stopPropagation();
+// });
