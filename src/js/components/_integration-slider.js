e913dd97091057945d0c60a2e63731df3830b940
Ihor Liubko <liub41k69@gmail.com>
2019-01-21 15:40:19 +0200

    responsive

--- src/js/components/_integration-slider.js
+++ src/js/components/_integration-slider.js
@@ -10,7 +10,31 @@ var slickOptions = {
   slidesToScroll: 2,
   arrows: true,
   prevArrow: ('<button type="button" class="slick-prev"><span></span></button>'),
-  nextArrow: ('<button type="button" class="slick-next"><span></span></button>')
+  nextArrow: ('<button type="button" class="slick-next"><span></span></button>'),
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true
+      }
+    },
+    {
+      breakpoint: 786,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
 };
 
 sliderSmall.slick(slickOptions);
