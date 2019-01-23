dbb6e7a88f485599272ec7fe5ccc9392eef7ee33
Ihor Liubko <liub41k69@gmail.com>
2019-01-23 13:20:47 +0200

    grid triple item

--- src/js/components/_select2.js
+++ src/js/components/_select2.js
@@ -3,5 +3,14 @@ import select2 from 'select2';
 var select = $('.js-select');
 
 select.select2({
-  minimumResultsForSearch: Infinity
+  minimumResultsForSearch: Infinity,
+  dropdownParent: $('.request__dropdown')
+});
+
+var selectNews = $('.js-select-news');
+
+selectNews.select2({
+  minimumResultsForSearch: Infinity,
+  dropdownParent: $('.news__dropdown'),
+  placeholder: 'Новости по всем направлениям'
 });
